e85b8f0e029f3756759ac4aa364d35da555951aa
Anurag <anuragraveendren98@gmail.com>
2023-12-18 10:19:29 +0530

    test2

--- index.js
+++ index.js
@@ -40,6 +40,29 @@ var max = function(list){
 }
 
 
+// ---------------------------------------------------------------------------
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.?????
+// "CodEWaRs" --> [0,3,4,6]
+
+var capitals = function (word) {
+    var capitalIndexes = [];
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === word[i].toUpperCase()) {
+        capitalIndexes.push(i);
+      }
+    }
+    return capitalIndexes;
+  };
+
+
+
+
+
+
+
+
+
+
 // Start the server
 const port = process.env.PORT || 3012;
 app.listen(port, () => {
