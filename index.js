2772dcdf0d05015f3f11813ae20d205ec1d94f99
Anurag <anuragraveendren98@gmail.com>
2023-12-19 10:26:28 +0530

    test3

--- index.js
+++ index.js
@@ -54,11 +54,59 @@ var capitals = function (word) {
     return capitalIndexes;
   };
 
+// ------------------------------------
+
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// A few examples:(Input1, Input2, Input3 --> Output)
+
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+
+// Try to do it without using if statements!
+// answer //
 
 
 
 
 
+// qstn
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  if( operation == "+") {
+    return value1 + value2
+  }
+  if( operation == "-") {
+    return value1 - value2
+  }
+  if( operation == "*") {
+    return value1 * value2
+  }
+  if( operation == "/") {
+    return value1 / value2
+  }
+}
 
 
 
