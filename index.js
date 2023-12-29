358d521b589d6eeb50208f91d640b6fd2e1e4230
Anurag <anuragraveendren98@gmail.com>
2023-12-29 09:23:29 +0530

    test25

--- index.js
+++ index.js
@@ -110,6 +110,35 @@ function basicOp(operation, value1, value2)
 
 
 
+function smallEnough(a, limit) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > limit) {
+      return false; 
+    }
+  }
+  return true; 
+}
+// ------------------------------------
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+// answer
+function addBinary(a,b) {
+  return (a + b).toString(2);
+}
+// -------------------------------
+
+
+
+
+
+
+
 
 // Start the server
 const port = process.env.PORT || 3012;
