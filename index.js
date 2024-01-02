3190da8c508a74f8cf51f2f870f48b4ee5c9c931
Anurag <anuragraveendren98@gmail.com>
2024-01-02 09:45:48 +0530

    test22

--- index.js
+++ index.js
@@ -259,6 +259,32 @@ function noBoringZeros(n) {
 }
 
 question
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task
+
+// Given a year, return the century it is in.
+// Examples
+
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+answer
+function century(year) {
+  // Finish this :)
+  
+    var x = Math.floor(year / 100);
+    if (year % 100 === 0)
+        return x;
+    else
+       
+  return x + 1;
+}
+
+question
+
+
+
 
 
 // Start the server
