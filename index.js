4f98bed96e77320e8683b0f8ac6dfd978488ac4f
Anurag <anuragraveendren98@gmail.com>
2024-01-03 09:40:05 +0530

    test26

--- index.js
+++ index.js
@@ -297,6 +297,25 @@ function problem(x){
 
 
 question
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+ 
+answer
+function distinct(a) {
+ 
+
+  return a.filter((item,
+      index) => a.indexOf(item) === index);
+}
+// console.log(removeDuplicates(a));
+
 
 
 
