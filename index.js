20d6298d3002af91d7d2faff7bc685aba1b2c092
Anurag <anuragraveendren98@gmail.com>
2024-01-04 09:55:02 +0530

    test30

--- index.js
+++ index.js
@@ -370,6 +370,23 @@ function updateLight(current) {
 }
 
 question 
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+// If you can, try writing it in one line of code
+
+
+answer
+const findDifference = (a, b) =>
+Math.abs(a.reduce((acc, val) =>
+ acc * val, 1) - b.reduce((acc, val) => acc * val, 1));
+
+ question 
+ 
+
 
 // Start the server
 const port = process.env.PORT || 3012;
