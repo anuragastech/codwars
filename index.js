647f722d3b812cd3cb2d37529038d3089792eb0d
Anurag <anuragraveendren98@gmail.com>
2024-01-05 09:48:25 +0530

    test39

--- index.js
+++ index.js
@@ -402,6 +402,21 @@ function stringToArray(string){
     return x;
 }
 
+question 
+// If you can't sleep, just count sheep!!
+// Task:
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+answers 
+var countSheep = function (num){
+
+  let x = "";
+  for (let i = 1; i <= num; i++) {
+      x += `${i} sheep...`;
+  }
+  return x.trim();
+}
 
 
 // Start the server
