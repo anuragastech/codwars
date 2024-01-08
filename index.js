fc565d66a3fd52e0a830fbfe8a485bc542f055c9
Anurag <anuragraveendren98@gmail.com>
2024-01-08 09:40:43 +0530

    test41

--- index.js
+++ index.js
@@ -442,6 +442,29 @@ function queueTime(customers, n) {
    return Math.max(...x);
 }
 questions 
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+answer 
+function accum(s) {
+	// your code
+
+  var letters = s.split(''), words = [];
+
+  for(var i = 0; i < letters.length; i++) {
+    words.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+
+  return words.join('-');
+}
+questions
+
 
 
 // Start the server
