ff4b2e24aea33b3ff801577734a82cd02a60f4a4
Anurag <anuragraveendren98@gmail.com>
2024-01-09 09:45:48 +0530

    test51

--- index.js
+++ index.js
@@ -488,6 +488,24 @@ function flattenAndSort(array) {
   return sorted;
 }
 
+question 
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+answer 
+var number=function(array){
+  //your awesome code here
+  return array.map((x, index) => `${index + 1}: ${x}`);
+}
+
 
 
 
