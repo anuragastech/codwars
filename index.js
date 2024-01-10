f2ee0677ae69ab698c5411d9b6d1fb811c4b6a5b
Anurag <anuragraveendren98@gmail.com>
2024-01-10 09:52:40 +0530

    test45

--- index.js
+++ index.js
@@ -528,6 +528,26 @@ function sumDigits(number) {
 }
 
 question 
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+answer 
+function persistence(num, count = 0) {
+  if (num < 10) {
+      return count;
+  } else {
+      const newvalue = num
+          .toString()
+          .split('')
+          .reduce((product, digit) => product * parseInt(digit), 1);
+
+      return persistence(newvalue, count + 1);
+  }
+}
 
 
 
