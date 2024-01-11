68d799c0c7e9e06c1031a9be34d21b0b79abcb83
Anurag <anuragraveendren98@gmail.com>
2024-01-11 09:33:49 +0530

    test55

--- index.js
+++ index.js
@@ -548,6 +548,42 @@ function persistence(num, count = 0) {
       return persistence(newvalue, count + 1);
   }
 }
+questions 
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// This is the first kata in series:
+
+//     Find the unique number (this kata)
+//     Find the unique string
+//     Find The Unique
+
+Answer 
+function findUniq(arr) {
+  // do magic
+
+    const count = {};
+
+    // Iterate through the array and count occurrences
+    for (let num of arr) {
+        count[num] = (count[num] || 0) + 1;
+    }
+
+    for (let num in count) {
+        if (count[num] === 1) {
+            return parseFloat(num); 
+        }
+    }
+}
+
+
+
 
 
 
