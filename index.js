80cae9a24d9f81979151c74e35cebb2babe2e5ac
Anurag <anuragraveendren98@gmail.com>
2024-01-12 09:42:53 +0530

    test56

--- index.js
+++ index.js
@@ -622,7 +622,26 @@ function digitalRoot(n) {
     return sum;
   }
 }
+questions 
+// Return the number (count) of vowels in the given string.
 
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+answer 
+function getCount(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 
 
