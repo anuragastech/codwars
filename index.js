46f90065d807c687175b8c6c59140fb8a300295d
Anurag <anuragraveendren98@gmail.com>
2024-01-12 09:37:20 +0530

    test55

--- index.js
+++ index.js
@@ -602,6 +602,26 @@ function isPangram(string) {
 }
 
 question 
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+answers 
+
+function digitalRoot(n) {
+  const digits = n.toString().split('').map(Number);
+
+  const sum = digits.reduce((acc, digit) => acc + digit, 0);
+
+  if (sum > 9) {
+    return digitalRoot(sum);
+  } else {
+    return sum;
+  }
+}
 
 
 
