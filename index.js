9d06e44dede24ae1c4d9e544b0e4895a067381f1
Anurag <anuragraveendren98@gmail.com>
2024-01-13 09:35:25 +0530

    test61

--- index.js
+++ index.js
@@ -664,6 +664,31 @@ function litres(time) {
   return x;
 }
 
+question 
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+answer 
+function past(h, m, s){
+
+  let seconds = s * 1000;
+  let minutes = m * 60000;
+  let hours = h * 3600000;
+
+  return seconds + minutes + hours;
+}
+
+
+
+
 
 
 
