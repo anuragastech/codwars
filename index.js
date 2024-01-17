a28c66b6e5e0a29b5bf56b6f627008b010aebc83
Anurag <anuragraveendren98@gmail.com>
2024-01-17 09:27:33 +0530

    54

--- index.js
+++ index.js
@@ -737,8 +737,24 @@ function reverse(string){
   return string.trim().split(/ +/).reverse().join(' ');
 }
 
+question 
+// When provided with a letter, return its position in the alphabet.
 
+// Input :: "a"
 
+// Ouput :: "Position of alphabet: 1"
+
+answer 
+
+function position(letter){
+  //Write your own Code!
+  
+      letter = letter.toLowerCase();  
+      var position = letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;  
+      return "Position of alphabet: " + position;
+  }
+  question 
+  
 
 
 
