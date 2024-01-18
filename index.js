54dbf76bfffc37d126b74c01babac2cefb6abd0d
Anurag <anuragraveendren98@gmail.com>
2024-01-18 09:35:06 +0530

    codwar33

--- index.js
+++ index.js
@@ -790,6 +790,30 @@ function sumTwoSmallestNumbers(numbers) {
 }
 
 
+question 
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+answer 
+function XO(str) {
+
+  str = str.toLowerCase();
+
+  const countX = (str.match(/x/g) || []).length;
+  const countO = (str.match(/o/g) || []).length;
+
+  return countX === countO;
+}
+
+
+
 
 
 
