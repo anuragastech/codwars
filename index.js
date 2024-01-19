a7f94278c64f06a878a36a992921dc0cf9b326b8
Anurag <anuragraveendren98@gmail.com>
2024-01-19 09:27:05 +0530

    65

--- index.js
+++ index.js
@@ -812,6 +812,28 @@ function XO(str) {
   return countX === countO;
 }
 
+question 
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+answers 
+
+const areaOrPerimeter = function(l , w) {
+  // Return your answer
+
+    if (l === w) {
+        return l * w;
+    } else {
+        return 2 * (l+ w);
+    }
+}
 
 
 
