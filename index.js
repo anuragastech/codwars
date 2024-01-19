a3fb336b5dafa5adf7f85f40472a13535cd07a9e
Anurag <anuragraveendren98@gmail.com>
2024-01-19 09:28:50 +0530

    66

--- index.js
+++ index.js
@@ -835,6 +835,33 @@ const areaOrPerimeter = function(l , w) {
     }
 }
 
+question
+// Your task, is to create N×N multiplication table, of size provided in parameter.
+
+// For example, when given size is 3:
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+// For the given example, the return value should be:
+
+// [[1,2,3],[2,4,6],[3,6,9]]
+answer 
+multiplicationTable = function(size) {
+
+  const table = [];
+
+  for (let i = 1; i <= size; i++) {
+      const row = [];
+      for (let j = 1; j <= size; j++) {
+          row.push(i * j);
+      }
+      table.push(row);
+  }
+
+  return table;
+}
 
 
 
