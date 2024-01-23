5d8ebec49e30e0b0a26438ce03ccf44724b215f4
Anurag <anuragraveendren98@gmail.com>
2024-01-23 09:13:58 +0530

    test51

--- index.js
+++ index.js
@@ -922,6 +922,37 @@ function cockroachSpeed(s) {
 
 
 
+question 
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+// Examples:
+
+// Input -> Output
+// 1,2,2 -> true
+// 4,2,3 -> true
+// 2,2,2 -> true
+// 1,2,3 -> false
+// -5,1,3 -> false
+// 0,2,3 -> false
+// 1,2,9 -> false 
+
+
+Answer 
+function isTriangle(a, b, c) {
+  // Check if all sides are positive
+  if (a > 0 && b > 0 && c > 0) {
+      // Check if the sum of any two sides is greater than the third side
+      if (a + b > c && b + c > a && c + a > b) {
+          return true;
+      }
+  }
+  return false;
+}
+
+
 
 
 
